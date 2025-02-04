3ae87ac69f06c5df143127f318e1c51ad1ba23a6
JoaoBarros <jpedrocbarros@gmail.com>
2025-02-04 20:01:56 -0300

    endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,26 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+    });
+    await client.connect();
+
+    try {
+        const result = await client.query(queryObject);
+        return result;
+    } catch (error) {
+        console.error("Error executing query", error);
+        throw error;
+    } finally {
+        await client.end();
+    }
 }
 
 export default {
-  query: query,
+    query: query,
 };
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,52 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ status: "ok" });
+    const updatedAt = new Date().toISOString();
+
+    const dataBaseVersionResult = await database.query({
+        text: "SHOW server_version;",
+    });
+
+    const databaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+
+    const databaseMaxConnectionsResult = await database.query({
+        text: "SHOW max_connections;",
+    });
+    const databaseMaxConnectionsValue = parseInt(
+        databaseMaxConnectionsResult.rows[0].max_connections,
+    );
+
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseOpenedConnectionsResult = await database.query({
+        text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+        values: [databaseName],
+    });
+    const databaseOpenedConnectionsValue =
+        databaseOpenedConnectionsResult.rows[0].count;
+
+    console.log(
+        "databaseOpenedConnectionsValue",
+        databaseOpenedConnectionsValue,
+    );
+
+    // Terminate all connections except the current one
+    // await database.query(`
+    //     SELECT pg_terminate_backend(pg_stat_activity.pid)
+    //     FROM pg_stat_activity
+    //     WHERE pg_stat_activity.datname = 'postgres'
+    //       AND pid <> pg_backend_pid();
+    //   `);
+
+    response.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: databaseVersionValue,
+                max_connections: databaseMaxConnectionsValue,
+                opened_connections: databaseOpenedConnectionsValue,
+            },
+        },
+    });
 }
 
 export default status;
